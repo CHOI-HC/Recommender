834baf46b43397f8983e485a5f98340fb0259a1f
CHOI-HC <choihchl@gmail.com>
2023-01-03 15:43:11 +0900

    ref

--- surprise_recommand.py
+++ surprise_recommand.py
@@ -10,11 +10,11 @@ from surprise import accuracy
 from surprise import Reader
 
 #%%
-#안 본 영화 id 추출
+# 안 본 영화 id 추출
 def getUnseenSurprise(df, userId):
-    seenMovies = df[df["userId"]==userId]["movieId"].tolist() #본 영화
-    allMovies = movies["movieId"].tolist() #전체 영화
-    unseenMovies = [movie for movie in allMovies if movie not in seenMovies] #안 본 영화
+    seenMovies = df[df["userId"]==userId]["movieId"].tolist()  # 본 영화
+    allMovies = movies["movieId"].tolist()  # 전체 영화
+    unseenMovies = [movie for movie in allMovies if movie not in seenMovies]  # 안 본 영화
     print(f"평점을 매긴 영화 수: {len(seenMovies)}, \n전체 영화 수: {len(allMovies)}, \n추천해야 할 영화 수: {len(unseenMovies)}")
     return unseenMovies
 
@@ -31,7 +31,7 @@ def surpriseRecMovie(algo, userId, unseenMovies, topN):
     topNpred = predictions[:topN]
     print(topNpred)
 
-    #top10으로 추출된 영화의 정보 추출(movieId, est, title)
+    # top10으로 추출된 영화의 정보 추출(movieId, est, title)
     topMovieIds = [int(pred.iid) for pred in topNpred]
     topMovieEsts = [pred.est for pred in topNpred]
     topMovieTitles = movies[movies["movieId"].isin(topMovieIds)]['title']
@@ -42,29 +42,29 @@ def surpriseRecMovie(algo, userId, unseenMovies, topN):
     return topMovieDf
 
 # %%
-###DatasetAutoFolds
-#trainset 정의
+### DatasetAutoFolds
+# trainset 정의
 reader = Reader(line_format='user item rating', sep=',', rating_scale=(0.5, 5))
 daf = DatasetAutoFolds(ratings_file = r'data/ratings_noh', reader=reader)
 trainset = daf.build_full_trainset()
 
-#알고리즘 정의
+# 알고리즘 정의
 algo = SVD(n_epochs=20, n_factors=50, random_state=0)
 algo.fit(trainset)
 
-###testest 정의
+### testest 정의
 movies = pd.read_csv(r"data/grouplens_movies.csv")
 ratings = pd.read_csv(r"data/grouples_ratings.csv")
 print(f"movies shape: {movies.shape}")
 print(f"ratings shape: {ratings.shape}")
 
-###9번 사용자에게 영화를 추천
-#What? 9번 사용자가 평점을 매기지 않은 영화 찾기
+
+### 9번 사용자에게 영화를 추천
+# What? 9번 사용자가 평점을 매기지 않은 영화 찾기
 
 # 9번 사용자가 보지 않은 영화들의 id
-unseenMovies = getUnseenSurprise(ratings, 9) #9번 사용자의 안 본 영화들의 id
+unseenMovies = getUnseenSurprise(ratings, 9)  # 9번 사용자의 안 본 영화들의 id
 unseenMovies
 
 # 9번 사용자
 surpriseRecMovie(algo, 9, unseenMovies, topN=10)
-# %%
--- surprise_review.py
+++ surprise_review.py
@@ -5,42 +5,42 @@ from surprise import accuracy
 from surprise.model_selection import train_test_split
 
 #%%
-#데이터 정의
+# 데이터 정의
 data = Dataset.load_builtin('ml-100k')
 trainset, testset = train_test_split(data, test_size=.25, random_state=0)
 
 # %%
-#잠재요인 협업필터링 수행
-#Process: 알고리즘 정의 > 학습 > 예측(test: 전체 데이터셋, predict: 개별 사용자)
+# 잠재요인 협업필터링 수행
+# Process: 알고리즘 정의 > 학습 > 예측(test: 전체 데이터셋, predict: 개별 사용자)
 
-#알고리즘 정의 및 학습
-algo = SVD() #행렬 분해
-algo.fit(trainset) #데이터 학습
+# 알고리즘 정의 및 학습
+algo = SVD()  # 행렬 분해
+algo.fit(trainset)  # 데이터 학습
 
 #%%
-#전체 예측: test
-predictions = algo.test(testset) #테스트 전체에 대한 추천영화 평점 데이터 반환
+# 전체 예측: test
+predictions = algo.test(testset)  # 테스트 전체에 대한 추천영화 평점 데이터 반환
 print("prediction type:", type(predictions), "size:", len(predictions))
 print("prediction 결과의 최초 5개 추출")
 print(predictions[:5])
 
-#반환된 객체 값 가져오기
+# 반환된 객체 값 가져오기
 [(pred.uid, pred.iid, pred.est) for pred in predictions[:3]]
 
 # %%
-#개별 예측: predict(uid(문자열), iid(문자열), r_ui(선택사항))
-uid = str(196) #196번 사용자
-iid = str(302) #302번 아이템
+# 개별 예측: predict(uid(문자열), iid(문자열), r_ui(선택사항))
+uid = str(196)  # 196번 사용자
+iid = str(302)  # 302번 아이템
 
 pred = algo.predict(uid, iid)
 print(pred)
 
 #%%
-#정확도 평가: 실제 평점(r_ui)과 예측 평점(est)과의 차이를 평가
+# 정확도 평가: 실제 평점(r_ui)과 예측 평점(est)과의 차이를 평가
 accuracy.rmse(predictions)
 
 # %%
-#주요API
+# 주요API
 # 1. Dataset.load_builtin: 데이터 내려 받기
 # 2. Dataset.load_from_file(file_path, reader): os파일에서 데이터를 로딩 
 #     - file_path: os파일명
@@ -50,18 +50,18 @@ accuracy.rmse(predictions)
 #     - reader: 파일의 포맷 
 
 #%%
-#os파일 데이터 surprise 데이터셋으로 로딩
+# os파일 데이터 surprise 데이터셋으로 로딩
 import pandas as pd
 
-#데이터 불러오기
+# 데이터 불러오기
 ratings = pd.read_csv(r'data/grouples_ratings.csv')
 
-#컬럼명 제거 후 데이터 저장
+# 컬럼명 제거 후 데이터 저장
 ratings.to_csv('/Users/choihc/Desktop/hcbigdata/portfolio/recommand/data/ratings_noh', index=False, header=False)
 
 #%%
-#os파일 불러오기
-#os파일을 불러오기 위해서는 불러오려는 기존 데이터프레임의 컬럼, 평점 관련 정보를 입력해야 함
+# os파일 불러오기
+# os파일을 불러오기 위해서는 불러오려는 기존 데이터프레임의 컬럼, 평점 관련 정보를 입력해야 함
 from surprise import Reader
 reader = Reader(line_format='user item rating timestamp', sep=',', rating_scale=(0.5, 5))
 data = Dataset.load_from_file(r'data/ratings_noh', reader=reader)
@@ -97,14 +97,14 @@ accuracy.rmse(predictions)
 # biased = 베이스라인 사용자 편향 적용 여부, default=True
 
 #%%
-#Baseline 평점: 사용자의 성향을 반영하여 평점 계산
+# Baseline 평점: 사용자의 성향을 반영하여 평점 계산
     # Baseline 평점 = (전체 평균 평점) + (사용자 편향 점수) + (아이템 편향 점수)
     # 전체 평균 평점: 모든 사용자의 모든 아이템에 대한 평점을 평균한 값
     # 사용자 편향 점수: (사용자별 아이템 평점 평균) - (전체 평균 평점)
     # 아이템 편향 점수: (아이템별 평균 평점) - (전체 평균 평점)
 
 #%%
-#Surprise 교차검증 및 하이퍼파라미터 튜닝 cross_validate
+# Surprise 교차검증 및 하이퍼파라미터 튜닝 cross_validate
 import pandas as pd
 
 from surprise import SVD
@@ -122,7 +122,7 @@ data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader=rea
 algo = SVD()
 cvDict = cross_validate(algo=algo, data=data, measures=["RMSE", "MAE"], cv=5, verbose=False)
 cvDict_df = pd.DataFrame(cvDict)
-cvDict_df = cvDict_df.T #T=transpose()
+cvDict_df = cvDict_df.T  # T=transpose()
 cvDict_df.columns = ["Fold1", "Fold2", "Fold3", "Fold4", "Fold5"]
 cvDict_df
 
